1cbadd834eeed9d268f4c641ea43246a498e3e8e
Tyler <tyler.roberts93@outlook.comgit config --global user.email tyler.roberts93@outlook.com>
2018-07-25 14:21:51 -0500

    Tested starter code for functionality

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -32,6 +32,9 @@ $(function() {
          * and that the URL is not empty.
          */
 
+         it('url defined', function() {
+         });
+
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
