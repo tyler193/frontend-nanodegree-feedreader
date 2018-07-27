1babf8e1a1e83268bb1fbaaa4ed411d1aac54da3
Tyler <tyler.roberts93@outlook.comgit config --global user.email tyler.roberts93@outlook.com>
2018-07-27 11:11:31 -0500

    Initial entries test works

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -86,6 +86,7 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -93,11 +94,22 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         beforeEach(function(done) {
+           loadFeed(0, done);
+         });
+
+         it('loadFeed complete', function() {
+           let feedTest = document.querySelector('.feed');
+           expect(feedTest.children.length > 0).toBe(true);
+         });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
